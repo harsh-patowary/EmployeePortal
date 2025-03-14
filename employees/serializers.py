8cfc195b1913ba6a4c81ae4c674775eecffacba5
hp22862 <hp22862@essex.ac.uk>
2025-03-14 00:51:13 +0000

    First Commit

---
+++ employees/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from .models import Employee
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'is_admin']
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Employee
+        fields = '__all__'
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
---
+++ employees/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import RegisterView, LoginView, EmployeeListCreateView, EmployeeDetailView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('employees/', EmployeeListCreateView.as_view(), name='employee-list'),
+    path('employees/<int:pk>/', EmployeeDetailView.as_view(), name='employee-detail'),
+]
